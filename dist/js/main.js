48e062ea78fe5c495de0256c8aa8d5887e713891
Andrey Sokolow <quick.sokolow94@gmail.com>
2019-03-13 19:00:02 +0300

    main almost finished

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-  
+  const ps = new PerfectScrollbar('.campaings-inn');
+
+  const headerMenu = function() {
+  	$(this).find('.header-menu')
+  				 .slideToggle(200);
+  };
+
+  const headerNav = function() {
+  	$(this).addClass('active')
+  				 .siblings()
+  				 .removeClass('active');
+  };
+
+  const mainBlockNav = function() {
+  	$(this).addClass('active')
+  				 .siblings()
+  				 .removeClass('active');
+  };
+
+  $(document).on('click', '.main-block-nav__link', mainBlockNav);
+  $(document).on('click', '.header-menu-container', headerMenu);
+  $(document).on('click', '.header .header-link:not(active)', headerNav);
 })
