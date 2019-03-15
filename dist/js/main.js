3d0166aa1a4f353d82636d6346692663ddb16c5a
Andrey Sokolow <quick.sokolow94@gmail.com>
2019-03-15 18:59:01 +0300

    saved changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
-  const ps = new PerfectScrollbar('.campaings-inn');
+  const psItems = $('.campaings-inn');
+  const ps1 = new PerfectScrollbar(psItems[0]);
+  const ps2 = new PerfectScrollbar(psItems[1]);
+  const ps3 = new PerfectScrollbar(psItems[2]);
 
-  const headerMenu = function() {
+  const closeHeaderMenu = function(e) {
+    if (!e.target.closest('.header-menu') && !e.target.closest('.header-menu-container')) {
+      $('.header-menu').slideUp(300);
+    }
+  };
+
+  const headerMenu = function(e) {
   	$(this).find('.header-menu')
   				 .slideToggle(200);
   };
@@ -18,7 +27,47 @@ $(document).ready(function(){
   				 .removeClass('active');
   };
 
+  const inputFocusOn = function() {
+    $(this).addClass('active');
+
+  };
+
+  const inputFocusOff = function() {
+    if ($(this).find('input').val().length) {
+      return false;  
+    }
+
+    $(this).removeClass('active');
+  };
+
+  const toLocaleString = function() {
+    let oldValue = $(this).val();
+
+    oldValue = oldValue.replace(/\s+/g,'');
+    oldValue = parseInt(oldValue);
+
+    $(this).val(oldValue.toLocaleString());
+  };
+
+  const changeTabs = function(e) {
+    e.preventDefault();
+    $(this).addClass('active')
+           .siblings()
+           .removeClass('active');
+
+    $(this).closest('.main-block')
+           .find('.main-block-tabs-item')
+           .removeClass('active')
+           .eq($(this).index())
+           .addClass('active');
+  }
+
   $(document).on('click', '.main-block-nav__link', mainBlockNav);
   $(document).on('click', '.header-menu-container', headerMenu);
   $(document).on('click', '.header .header-link:not(active)', headerNav);
-})
+  $(document).on('focus', '.input-text', inputFocusOn);
+  $(document).on('blur', '.input-text', inputFocusOff);
+  $(document).on('change', '.input-text input', toLocaleString);
+  $(document).on('click', '[data-tabs-link]:not(.active)', changeTabs);
+  $(document).on('click', closeHeaderMenu);
+});
