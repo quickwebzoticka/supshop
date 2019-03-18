e84d97508dbd45b434e711cdf04f37b027c0cd39
Andrey Sokolow <quick.sokolow94@gmail.com>
2019-03-18 18:59:02 +0300

    faq in progress

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,30 @@
 $(document).ready(function(){
-  const psItems = $('.campaings-inn');
-  const ps1 = new PerfectScrollbar(psItems[0]);
-  const ps2 = new PerfectScrollbar(psItems[1]);
-  const ps3 = new PerfectScrollbar(psItems[2]);
+  $('.campaings-inn-content').scrollbar();
+  $('.address-list').scrollbar();
 
   const closeHeaderMenu = function(e) {
     if (!e.target.closest('.header-menu') && !e.target.closest('.header-menu-container')) {
       $('.header-menu').slideUp(300);
     }
   };
+
+  const moveToAnchor = function() {
+    let pathname = location.href;
+
+    if (pathname.indexOf('#') > 0) {
+      pathname = pathname.split('#');
+      pathname = pathname[(pathname.length - 1)];
+
+      $('html, body').scrollTop(0);
+      $('.registration').removeClass('active');
+      $('body').css('overflow', '');
+
+      let target = $(`#${pathname}`).offset().top - 150;
+
+      $('html, body').animate({scrollTop: target}, 1000);
+      return false;
+    }
+  }
 
   const headerMenu = function(e) {
   	$(this).find('.header-menu')
@@ -33,9 +49,19 @@ $(document).ready(function(){
   };
 
   const inputFocusOff = function() {
-    if ($(this).find('input').val().length) {
-      return false;  
+    if ($(this).find('input').length) {
+      if ($(this).find('input').val().length) {
+        return false;  
+      }
     }
+    
+
+    if ($(this).find('textarea').length) {
+      if ($(this).find('textarea').val().length) {
+        return false;  
+      }
+    }
+    
 
     $(this).removeClass('active');
   };
@@ -60,14 +86,72 @@ $(document).ready(function(){
            .removeClass('active')
            .eq($(this).index())
            .addClass('active');
-  }
+  };
 
+  const scrollToBlock = function(e) {
+    e.preventDefault();
+    let link = $(this).attr('href');
+    if (window.location.pathname == '/index.html') {
+      if (link.indexOf('#') <= 0) return false;
+      
+      link = link.split("#");
+      link = `#${link[(link.length - 1)]}`;
+
+      if (!$(document).find(`${link}`).length) {
+        return false;
+      }
+
+      let targetOffset = ($(document).find(`${link}`).offset().top - 150);
+
+      $('header .header-link').css('pointer-events', 'none');
+      setTimeout(function() {
+        $('header .header-link').css('pointer-events', 'auto');
+      },1000);
+
+      $('html, body').animate({scrollTop: targetOffset}, 1000);
+
+      return false;
+    } else {
+      window.location = `${link}`;
+    }
+  };
+
+  const removePopup = function(e) {
+    e.preventDefault();
+
+    $('body').css('overflow', '');
+
+    $(this).closest('.active')
+           .removeClass('active');
+  };
+
+  const removeActiveLinks = function() {
+    if (window.location.pathname !== '/index.html') {
+      $('header .header-link').removeClass('active');
+    }
+  };
+
+  const activeRadio = function() {
+    $(this).closest('.form-input').addClass('active');
+  };
+
+  removeActiveLinks();
+  moveToAnchor();
+
+  $(document).on('click', '.form-input__radio', activeRadio);
+  $(document).on('click', '.registration .btn-cancel', removePopup);
+  $(document).on('click', 'header .header-link', scrollToBlock);
   $(document).on('click', '.main-block-nav__link', mainBlockNav);
   $(document).on('click', '.header-menu-container', headerMenu);
   $(document).on('click', '.header .header-link:not(active)', headerNav);
-  $(document).on('focus', '.input-text', inputFocusOn);
-  $(document).on('blur', '.input-text', inputFocusOff);
-  $(document).on('change', '.input-text input', toLocaleString);
   $(document).on('click', '[data-tabs-link]:not(.active)', changeTabs);
   $(document).on('click', closeHeaderMenu);
+  $(document).on('change', '.input-text input', toLocaleString);
+  $(document).on('focus', '.input-text', inputFocusOn);
+  $(document).on('focus', '.form-input', inputFocusOn);
+  $(document).on('blur', '.input-text', inputFocusOff);
+  $(document).on('blur', '.form-input', inputFocusOff);
+  
+
+
 });
