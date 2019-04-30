6dfff3a8d5f05f048cbb80014e0fa34514eef066
Andrey Sokolow <quick.sokolow94@gmail.com>
2019-04-30 20:29:48 +0300

    1

--- src/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$sol = "ufsfxxx";
-$PASSWORD = $_POST["PASSWORD"];
-$LOGIN = $_POST["LOGIN"];
-$str = $LOGIN.$sol.$PASSWORD;
-$massData = [ "USERID" => time(), "TOKEN" => md5($str) ];
-echo json_encode($massData);
